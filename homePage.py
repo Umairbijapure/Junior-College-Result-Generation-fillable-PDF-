e8fb169639ad70091b19e5a4419affde03dac666
Umer Bijapure <90462177+Umairbijapure@users.noreply.github.com>
2022-07-02 18:26:58 +0530

    Update homePage.py
    Here,we can add students manually by fill their details into the entries from the button "Add Student".
    also there is a special feature, if you have excel file containing students data u can directly import that into this software
    then, we can add respective subjects chosen by students by clicking add subjects button
    then, we provide marks to each student.
    then again u lastly go for result and consolidated sheet generations.

--- homePage.py
+++ homePage.py
@@ -1,72 +1,94 @@
+import sqlite3
 from tkinter import *
-from PIL import Image,ImageTk
+from tkinter import messagebox
+
+from PIL import Image, ImageTk
+
 from add11stdA import add11stdA
-from fillMarks import class_marks
 from course import course
+from fillMarks import class_marks
 from result import resultClass
+from postgress import csv_export
+
 class SSA:
-    def __init__(self,window):
-        self.window=window
+    def __init__(self, window):
+        self.window = window
         self.window.title("SSA Result Management System")
-        self.window.geometry("1350x700+80+170")
-        self.window.config(bg="white")
-        self.window.focus_force()
-        #ICONS
-        img=(Image.open("logo.png"))
-        resized_image=img.resize((60,60))
-        self.logo_dash=ImageTk.PhotoImage(resized_image)
-        title=Label(self.window,text="SSA Result Management System",padx=10,compound=LEFT,image=self.logo_dash,font=("goudy old stlye",20,"bold"),bg="#87CEFA",fg="White").place(x=0,y=0,relwidth=1,height=50)
-        #choos std
-        frame_menu=LabelFrame(self.window,text="Menus",font=("time new roman",15),bg="white")
-        frame_menu.place(x=10,y=70,width=190,height=508)
+        self.window.geometry("1350x800+80+50")
+        self.window.config(bg="#eBffff")
 
-        #btn_std=Button(frame_menu,text="Standard",font=("goudym old style",15),bg="#0b5377",fg="white",cursor="hand2",command=self.add_standard).place(x=7,y=10,width=170,height=35)
-        btn_course = Button(frame_menu, text="Add Subjects", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.add_course).place(x=7, y=10, width=170, height=35)
-        btn_student = Button(frame_menu, text="Add Student", font=("goudym old style", 15), bg="#0b5377", fg="white", cursor="hand2",command=self.add_student).place(x=7, y=60, width=170, height=35)
-        btn_fill_marks = Button(frame_menu, text="Store Marks", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2",command=self.fill_marks).place(x=7, y=110, width=170, height=35)
-        btn_result = Button(frame_menu, text="Result", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2",command=self.result).place(x=7, y=160, width=170, height=35)
-        btn_logout = Button(frame_menu, text="Logout", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2").place(x=7, y=210, width=170, height=35)
-
-        #Image
-        self.image1=Image.open("SSA3.jpg")
-        self.image1=self.image1.resize((920,350))
-        self.image1=ImageTk.PhotoImage(self.image1)
-        self.label_image1=Label(self.window,image=self.image1).place(x=370,y=60,width=600,height=500)
-
-        #Content details
-        self.label_student=Label(self.window,text="Total Students \n[ 0 ]",font=("goudy old style",20),bd=10)
-        self.label_student.place(x=370,y=585,width=255,height=80)
-
-        self.label_result = Label(self.window, text="Total Results \n[ 0 ]", font=("goudy old style", 20),bd=10)
-        self.label_result.place(x=716, y=585, width=255, height=80)
+        # ICONS
+        img = (Image.open("logo.png"))
+        resized_image = img.resize((60, 60))
+        self.logo_dash = ImageTk.PhotoImage(resized_image)
+        title = Label(self.window, text="SSA Result Management System", padx=10, compound=LEFT, image=self.logo_dash,font=("goudy old stlye", 20, "bold"), bg="#87CEFA", fg="White").place(x=0, y=0, relwidth=1,height=50)
+        #menu
+        frame_menu = LabelFrame(self.window, text="Menus", font=("time new roman", 15), bg="#eBffff")
+        frame_menu.place(x=10, y=70, width=190, height=508)
 
 
+        btn_course = Button(frame_menu, text="Add Subjects", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.add_course).place(x=7, y=60, width=170, height=35)
+        btn_student = Button(frame_menu, text="Add Student", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.add_student).place(x=7, y=10, width=170, height=35)
+        btn_fill_marks = Button(frame_menu, text="Store Marks", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.fill_marks).place(x=7, y=110, width=170, height=35)
+        btn_result = Button(frame_menu, text="Result", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.result).place(x=7, y=160, width=170, height=35)
+        btn_all_students = Button(frame_menu, text="All Students", font=("goudym old style", 15), bg="#0b5377", fg="white",cursor="hand2", command=self.all_students).place(x=7, y=210, width=170, height=35)
+        #btn_logout = Button(frame_menu, text="About", font=("goudym old style", 15), bg="#0b5377", fg="white",
+                            #cursor="hand2").place(x=7, y=210, width=170, height=35)
+
+        # Image
+        self.image1 = Image.open("SSA3.jpg")
+        #self.image1 = self.image1.resize((920, 350))
+        self.image1 = ImageTk.PhotoImage(self.image1)
+        self.label_image1 = Label(self.window, image=self.image1,bg="#eBffff").place(x=370, y=60, width=900, height=800)
+
+        # Content details
+        self.label_student = Label(self.window, text="Total Students \n[ 0 ]", font=("goudy old style", 20), bd=10, bg="#eBffff")
+        self.label_student.place(x=370, y=585, width=255, height=80)
+
+        #self.label_result = Label(self.window, text="Total Results \n[ 0 ]", font=("goudy old style", 20), bd=10, bg="#eBffff")
+        #self.label_result.place(x=716, y=585, width=255, height=80)
+
+        # Footer
+        title = Label(self.window,
+                      text="SSA Result Management System\nContact US for any technical issue:Umer Bijapure-7020152273",
+                      font=("goudy old stlye", 10), bg="#87CEFA", fg="White").pack(side=BOTTOM, fill=X)
+        self.total_students()
+    def total_students(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor()
+        try:
+            cur.execute("select * from studentTable")
+            rows1 = cur.fetchall()
+            self.label_student.config(text=f"Total Students[{str(len(rows1))}]")
+            self.label_student.after(200,self.total_students)
+
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
 
 
-
-
-        #Footer
-        title = Label(self.window, text="SSA Result Management System\nContact US for any technical issue:Umer Bijapure-7020152273",font=("goudy old stlye", 10), bg="#87CEFA", fg="White").pack(side=BOTTOM,fill=X)
     def add_course(self):
-        self.new_win=Toplevel(self.window)
-        self.obj1=course(self.new_win)
+        self.new_win = Toplevel(self.window)
+        self.obj1 = course(self.new_win)
 
     def add_student(self):
-        self.new_win=Toplevel(self.window)
-        self.obj2=add11stdA(self.new_win)
+        self.new_win = Toplevel(self.window)
+        self.obj2 = add11stdA(self.new_win)
+
     def fill_marks(self):
-        self.new_win=Toplevel(self.window)
-        self.obj2=class_marks(self.new_win)
+        self.new_win = Toplevel(self.window)
+        self.obj2 = class_marks(self.new_win)
+
     def result(self):
-        self.new_win=Toplevel(self.window)
-        self.obj2=resultClass(self.new_win)
+        self.new_win = Toplevel(self.window)
+        self.obj2 = resultClass(self.new_win)
+
+    def all_students(self):
+        self.new_win = Toplevel(self.window)
+        self.obj2 = csv_export(self.new_win)
 
 
 
-
-
-
-if __name__=="__main__":
-    window=Tk()
-    obj1=SSA(window)
+if __name__ == "__main__":
+    window = Tk()
+    obj1 = SSA(window)
     window.mainloop()
